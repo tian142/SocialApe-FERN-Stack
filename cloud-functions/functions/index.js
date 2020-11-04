f135966084e248e817bf77a34c5e97c5688f5120
Tian Qiu <tian.qiu142@gmail.com>
2020-11-04 14:26:48 -0800

    completed validation for sign up

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -15,6 +15,7 @@ const config = {
 };
 
 const firebase = require('firebase');
+const { UserBuilder } = require('firebase-functions/lib/providers/auth');
 firebase.initializeApp(config);
 
 const db = admin.firestore();
@@ -53,6 +54,17 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+
 //signup routre
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -61,6 +73,20 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+
+  let errors = {};
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = 'Passwords must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
