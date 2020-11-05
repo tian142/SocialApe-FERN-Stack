1273b5e311ce96ed3158a38298dbc47e4328818d
Tian Qiu <tian.qiu142@gmail.com>
2020-11-04 18:33:24 -0800

    refactored functions

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,130 +1,18 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
 const app = require('express')();
-admin.initializeApp();
 
-const config = {
-  apiKey: 'AIzaSyCjWtA3ElW98G3Wu1GeKI2dAudDtWLW1UA',
-  authDomain: 'social-ape-cad2a.firebaseapp.com',
-  databaseURL: 'https://social-ape-cad2a.firebaseio.com',
-  projectId: 'social-ape-cad2a',
-  storageBucket: 'social-ape-cad2a.appspot.com',
-  messagingSenderId: '153586042537',
-  appId: '1:153586042537:web:e7d295855d7dc39ba34bb8',
-};
+const FBAuth = require('./util/FBAuth');
 
-const firebase = require('firebase');
-const { UserBuilder } = require('firebase-functions/lib/providers/auth');
-firebase.initializeApp(config);
+const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { signup, login } = require('./handlers/users');
 
-const db = admin.firestore();
+//Scream Routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
 
-app.get('/screams', (req, res) => {
-  db.collection('screams')
-    .get()
-    .then((data) => {
-      let screams = [];
-      data.forEach((doc) => {
-        screams.push({
-          screamId: doc.id,
-          ...doc.data(),
-        });
-      });
-      return res.json(screams);
-    })
-    .catch((err) => console.error(err));
-});
-
-app.post('/scream', (req, res) => {
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString(),
-  };
-
-  db.collection('screams')
-    .add(newScream)
-    .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: 'something went wrong' });
-      console.error(err);
-    });
-});
-
-const isEmail = (email) => {
-  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(regEx)) return true;
-  else return false;
-};
-
-const isEmpty = (string) => {
-  if (string.trim() === '') return true;
-  else return false;
-};
-
-//signup routre
-app.post('/signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
-
-  let errors = {};
-  if (isEmpty(newUser.email)) {
-    errors.email = 'Must not be empty';
-  } else if (!isEmail(newUser.email)) {
-    errors.email = 'Must be a valid email address';
-  }
-
-  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
-  if (newUser.password !== newUser.confirmPassword)
-    errors.confirmPassword = 'Passwords must match';
-  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: 'this handle is already taken' });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
-    .then((data) => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then((idToken) => {
-      token = idToken;
-      const userCredentials = {
-        handle: newUser.handle,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId,
-      };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(() => {
-      return res.status(201).json({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'email is already in use' });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
-    });
-});
+//Users Routes
+app.post('/signup', signup);
+app.post('/login', login);
 
 exports.api = functions.https.onRequest(app);
---
+++ cloud-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyCjWtA3ElW98G3Wu1GeKI2dAudDtWLW1UA',
+  authDomain: 'social-ape-cad2a.firebaseapp.com',
+  databaseURL: 'https://social-ape-cad2a.firebaseio.com',
+  projectId: 'social-ape-cad2a',
+  storageBucket: 'social-ape-cad2a.appspot.com',
+  messagingSenderId: '153586042537',
+  appId: '1:153586042537:web:e7d295855d7dc39ba34bb8',
+};
